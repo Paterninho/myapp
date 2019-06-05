4bf651bd49ce66b4d66adf6ab6228a808fd30b17
Paterninho <anderson_paterno@estudante.sc.senai.br>
2019-06-05 20:25:21 -0300

    Update Mysql.js

--- db/Mysql.js
+++ db/Mysql.js
@@ -2,12 +2,12 @@ const mysql = require('mysql');
 var express = require('express');
 var app = express.Router();
 var optionConection = {
-  // host     : '10.3.79.235',
-  // user     : 'datasaude',
-  // password : 'sau09*123data',
-  host     : 'localhost',
-  user     : 'root',
-  password : 'password',
+  host     : '10.3.79.235',
+  user     : 'datasaude',
+  password : 'sau09*123data',
+  //host     : 'localhost',
+  //user     : 'root',
+  //password : 'password',
   database : 'datasaude'
 };
 app.post('/column', function(req, res) {
@@ -67,7 +67,27 @@ app.post('/line', function(req, res) {
     });
 
   }
-  execSQLQuery('SELECT ROUND(sum((a.bitos / a.Dias_permanncia) * 100), 2) as "M", ROUND(sum((b.bitos / b.Dias_permanncia) * 100), 2) as "F" FROM DataSus_Line_Ano_Obitos_Mas_TodasIdades_2013_convertido a join DataSus_Line_Ano_Obitos_Fem_TodasIdades_2013_convertido b on (a.regio = b.regio)where a.Regio = "Total";', res); 
+  execSQLQuery('SELECT ROUND(sum((a.bitos / a.Dias_permanncia) * 100), 2) as "M", ROUND(sum((b.bitos / b.Dias_permanncia) * 100), 2) as "F" FROM DataSus_Line_Ano_Obitos_Mas_TodasIdades_2013_convertido a join DataSus_Line_Ano_Obitos_Fem_TodasIdades_2013_convertido b on (a.regio = b.regio) where a.Regio = "Total";', res); 
+})
+
+app.post('/ages', function(req, res) {
+  
+  function execSQLQuery(sqlQry, res){
+    const connection = mysql.createConnection(optionConection);
+    
+    connection.query(sqlQry, function(error, results, fields){
+      if(error){
+        res.json(error);
+      }else{
+        res.json(results);
+        console.log(results);
+        connection.end();
+      }
+        
+    });
+
+  }
+  execSQLQuery('SELECT a.table_name, b.table_name, SUBSTRING(a.table_name, 23, 9) as "idades", SUBSTRING(a.table_name, 14, 3) as "mes" FROM information_schema.tables a, information_schema.tables b WHERE a.table_schema = "datasaude" and a.table_schema = b.table_schema and a.table_name like "%fem%" and b.table_name like "%masc%" and SUBSTRING(a.table_name, 23, 9) = SUBSTRING(b.table_name, 23, 9) and SUBSTRING(a.table_name, 14, 3) = SUBSTRING(b.table_name, 14, 3)', res); 
 })
 
 app.post('/pie', function(req, res) {
